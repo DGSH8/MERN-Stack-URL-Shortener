9c7ce6b8a2ed5ff8b59cb3ec41dcaebb08cc49c2
DGSH8 <94004294+DGSH8@users.noreply.github.com>
2024-02-09 01:04:33 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -3,10 +3,22 @@ const redirect = require('./controllers/redirects.controller');
 const url = require('./controllers/url.controller');
 const EmployeeModel = require('./models/Employee')
 var cors = require('cors');
+const connect = require('./configs/db');
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.listen(PORT, async () => {
+    try {
+        await connect();
+        console.log(`Server is running on ${PORT}`);
+
+    } catch (error) {
+        console.log(error.message);
+    }
+});
 
 app.get('/', (req, res) => {
     res.status(200).json({ Message: 'Hi there' });
