3bb323661f223ad1af16fc971a1fd8edc2a30106
DGSH8 <94004294+DGSH8@users.noreply.github.com>
2024-02-09 01:52:37 +0530

    sixth commit

--- server/index.js
+++ server/index.js
@@ -8,13 +8,11 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors(
-    {
-        origin: ["https://mern-stack-url-shortener-frontend-theta.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+app.use(cors({
+    origin: '*', // Allow requests from all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allow specific headers
+}));
 app.use(express.json());
 app.listen(PORT, async () => {
     try {
